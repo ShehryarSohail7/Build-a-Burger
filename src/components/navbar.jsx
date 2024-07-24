e288c4d31265788844148f45eace022656a218a7
Shehryar Sohail <shehryar.sohail@devsinc.com>
2024-07-24 15:36:58 +0500

    navbar css corrected

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,104 +1,17 @@
 import React, { useState } from "react";
 import logo from "../images/logo.png";
 import { Link, useNavigate } from "react-router-dom";
+import "../components/css/navbar.css";
 
 const Navbar = (props) => {
   // Use States
-  const [highlight_builder, SetHighlight_builder] = useState({
-    paddingTop: "16px",
-    paddingBottom: "11px",
-    backgroundColor: "",
-    fontFamily: "Arial",
-    fontSize: "14px",
-    borderBottom: "",
-  });
-  const [highlight_login, SetHighlight_login] = useState({
-    paddingTop: "16px",
-    paddingBottom: "11px",
-    fontFamily: "Arial",
-    fontSize: "14px",
-    backgroundColor: "",
-    borderBottom: "",
-  });
-  const [highlight_order, SetHighlight_order] = useState({
-    paddingTop: "16px",
-    paddingBottom: "11px",
-    fontFamily: "Arial",
-    fontSize: "14px",
-    backgroundColor: "",
-    borderBottom: "",
-  });
+  const [active, setActive] = useState("");
 
   // Functions
 
-  const Click_Highlight_builder = () => {
-    SetHighlight_builder({
-      paddingTop: "16px",
-      paddingBottom: "11px",
-      fontFamily: "Arial",
-      fontSize: "14px",
-      backgroundColor: "#8f5c2c",
-      borderBottom: "4px solid #40a4c8",
-    });
-    SetHighlight_login({
-      paddingTop: "16px",
-      paddingBottom: "11px",
-      fontFamily: "Arial",
-      fontSize: "14px",
-      backgroundColor: "",
-      borderBottom: "",
-    });
-    SetHighlight_order({
-      paddingTop: "16px",
-      paddingBottom: "11px",
-      fontFamily: "Arial",
-      fontSize: "14px",
-      backgroundColor: "",
-      borderBottom: "",
-    });
-  };
-
-  const Click_Highlight_login = () => {
-    SetHighlight_login({
-      paddingTop: "16px",
-      paddingBottom: "11px",
-      fontFamily: "Arial",
-      fontSize: "14px",
-      backgroundColor: "#8f5c2c",
-      borderBottom: "4px solid #40a4c8",
-    });
-    SetHighlight_builder({
-      paddingTop: "16px",
-      paddingBottom: "11px",
-      fontFamily: "Arial",
-      fontSize: "14px",
-      backgroundColor: "",
-      borderBottom: "",
-    });
-  };
-
-  const Click_Highlight_order = () => {
-    SetHighlight_order({
-      paddingTop: "16px",
-      paddingBottom: "11px",
-      fontFamily: "Arial",
-      fontSize: "14px",
-      backgroundColor: "#8f5c2c",
-      borderBottom: "4px solid #40a4c8",
-    });
-    SetHighlight_builder({
-      paddingTop: "16px",
-      paddingBottom: "11px",
-      fontFamily: "Arial",
-      fontSize: "14px",
-      backgroundColor: "",
-      borderBottom: "",
-    });
-  };
-
   const navigate = useNavigate();
   const Handle_Logout = () => {
-    props.userSetter(null);
+    props.userSetter(null); // making the active user null because we are logging out
     navigate("/");
   };
 
@@ -118,51 +31,44 @@ const Navbar = (props) => {
 
       <div className="mx-4">
         <Link
-          className="px-2 text-white text-decoration-none"
-          style={highlight_builder}
+          className={`custom-link ${
+            active === "builder" ? "link-click" : ""
+          } px-2 text-white text-decoration-none`}
           to="/"
-          onClick={Click_Highlight_builder}
+          onClick={() => setActive("builder")}
         >
           Burger Builder
         </Link>
         <Link
-          className={`px-2 text-white text-decoration-none d-${
+          className={`custom-link ${
+            active === "signin" ? "link-click" : ""
+          } px-2 text-white text-decoration-none d-${
             props.activeUser === null ? "display" : "none"
           }`}
           to="/login"
-          style={highlight_login}
-          onClick={Click_Highlight_login}
+          onClick={() => setActive("signin")}
         >
           Sign In
         </Link>
         <Link
-          className={`px-2 text-white text-decoration-none d-${
+          className={`custom-link ${
+            active === "order" ? "link-click" : ""
+          } px-2 text-white text-decoration-none d-${
             props.activeUser === null ? "none" : "display"
           }`}
           to="/orders"
-          style={highlight_order}
-          onClick={Click_Highlight_order}
+          onClick={() => setActive("order")}
         >
           Orders
         </Link>
-        <button
-          className={`px-2 text-white text-decoration-none  d-${
+        <Link
+          className={`custom-link px-2 text-white text-decoration-none  d-${
             props.activeUser === null ? "none" : "display"
           }`}
-          style={{
-            paddingTop: "16px",
-            paddingBottom: "11px",
-            fontFamily: "Arial",
-            fontSize: "14px",
-            backgroundColor: "",
-            borderBottom: "",
-            background: "none",
-            border: "none",
-          }}
           onClick={Handle_Logout}
         >
           Log Out
-        </button>
+        </Link>
       </div>
     </nav>
   );
--- src/components/popup.jsx
+++ src/components/popup.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./popup.css";
+import "./css/popup.css";
 
 const Popup = (props) => {
   console.log("popup tiggered");
